59566b2b72c37314613f35f956bc373e21444183
BAHA Ephraim <jeansamist@gmail.com>
2023-07-26 17:06:19 +0100

    confirm context

--- src/Contexts/ConfirmContext.tsx
+++ src/Contexts/ConfirmContext.tsx
@@ -1,46 +1,32 @@
-// import { Button, Flexbox, Footer, Modal as ModalElement, ModalProps } from "@silva-school-frontend/ui";
-// import { ComponentProps, FunctionComponent, PropsWithChildren, ReactNode, createContext, useCallback, useState } from "react";
+import { Modal } from "@silva-school-frontend/ui";
+import { ComponentProps, Dispatch, FunctionComponent, PropsWithChildren, SetStateAction, createContext, useCallback, useState } from "react";
+type Confirm = { onConfirm?: () => void; onCancel?: () => void; setter?: Dispatch<SetStateAction<boolean>> } & ComponentProps<typeof Modal>;
+type _def = {
+  confirms: Confirm[];
+  pushConfirm: (confirm: Confirm) => void;
+};
+const def: _def = {
+  confirms: [],
+  pushConfirm: () => {
+    return;
+  },
+};
+function useConfirm(): _def {
+  const [confirms, setconfirms] = useState<Confirm[]>([
+    {
+      children: "Do you really want to perform this action?",
+    },
+  ]);
+  const pushConfirm = useCallback((confirm: Confirm) => {
+    confirm.setter !== undefined && confirm.setter(true);
+    // confirm.isVisible = true;
+    setconfirms((v) => [...v, confirm]);
+  }, []);
+  return { confirms, pushConfirm };
+}
 
-// type Modal = ComponentProps<typeof ModalElement>;
-
-// type _def = {
-//   confirms: Modal[];
-//   onConfirm: () => void;
-//   pushConfirm: (title: ReactNode, content: ReactNode) => void;
-// };
-
-// const def: _def = {
-//   confirms: [],
-//   onConfirm: () => {
-//     return;
-//   },
-//   pushConfirm: () => {
-//     return;
-//   },
-// };
-
-// function useConfirm(): _def {
-//   const [confirms, setconfirms] = useState<Modal[]>([
-//     {
-//       children: "Confirm your action",
-//       footer: (
-//         <Footer>
-//           <Flexbox>
-//             <Button onClick={}>Confirm</Button>
-//           </Flexbox>
-//         </Footer>
-//       ),
-//     },
-//   ]);
-//   const pushConfirm = useCallback((toast: Confirm) => {
-//     setconfirms((v) => [...v, toast]);
-//   }, []);
-//   return { confirms, pushConfirm };
-// }
-
-// export const ConfirmContext = createContext(def);
-
-// export const ConfirmProvider: FunctionComponent<PropsWithChildren> = ({ children }) => {
-//   const toast = useConfirm();
-//   return <ConfirmContext.Provider value={toast}>{children}</ConfirmContext.Provider>;
-// };
+export const ConfirmContext = createContext(def);
+export const ConfirmProvider: FunctionComponent<PropsWithChildren> = ({ children }) => {
+  const confirm = useConfirm();
+  return <ConfirmContext.Provider value={confirm}>{children}</ConfirmContext.Provider>;
+};
